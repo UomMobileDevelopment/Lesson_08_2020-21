d5fe5af7448ad2b24f126e02e9956296ae6ed3d9
Theodore <teohaik@gmail.com>
2018-12-07 22:37:03 +0200

    sucessfully added greenDAO library for ORM with SQLite

--- app/src/main/java/gr/uom/android/lesson_10/MainActivity.java
+++ app/src/main/java/gr/uom/android/lesson_10/MainActivity.java
@@ -1,7 +1,15 @@
 package gr.uom.android.lesson_10;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.util.Date;
+
+import gr.uom.android.lesson_10.model.DaoSession;
+import gr.uom.android.lesson_10.model.Note;
+import gr.uom.android.lesson_10.model.NoteDao;
+import gr.uom.android.lesson_10.model.NoteType;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DaoSession daoSession = ((App) getApplication()).getDaoSession();
+
+        NoteDao noteDao = daoSession.getNoteDao();
+
+        Note note = new Note();
+        note.setText("Some text");
+        note.setComment("A comment");
+        note.setDate(new Date());
+        note.setType(NoteType.TEXT);
+        noteDao.insert(note);
+        Log.d("DaoExample", "Inserted new note, ID: " + note.getId());
     }
+
 }
